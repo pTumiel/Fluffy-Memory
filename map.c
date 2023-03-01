9018e5d4155ba8e4f7dc62777bc4b6814f619c3b
Peter Tumiel <petertumiel@gmail.com>
2023-03-01 22:11:40 +0200

    Initial loop for map build

--- map.c
+++ map.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+
 int main(){
-	printf("a");
+
+	char x='x';
+	char charxy=x+'x';
+	printf("%c",charxy);	
+	while (true){
+	printf("%c\r",x);
+	x=x+'x';
+	}
 }
